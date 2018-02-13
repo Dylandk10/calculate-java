80dfab46499d39a09a9f273eb8caacc8761c7eb9
Dylan Kelly <dylankelly@Dylans-MBP.fios-router.home>
2018-02-13 13:29:59 -0500

    change main app button handling

--- src/App.java
+++ src/App.java
@@ -10,10 +10,26 @@ import javafx.geometry.Insets;
 import javafx.geometry.*;
 
 public class App extends Application{
+	Button button1 = new Button("1");
+	Button button2 = new Button("2");
+	Button button3 = new Button("3");
+	Button button4 = new Button("4");
+	Button button5 = new Button("5");
+	Button button6 = new Button("6");
+	Button buttonAdd = new Button("+");
+	Button buttonSubtract = new Button("-");
+	Button buttonEqual = new Button("=");
+	Button buttonClear = new Button("C");
+	Button buttonBinary = new Button("To Bianry");
+	
+	TextField inputField = new TextField();
+	Label input = new Label();
 
+	ButtonHandler buttonHandler = new ButtonHandler();
+	Calculate calculate = new Calculate();
+	
 	public static void main(String[] args) {
 		launch(args);
-
 	}
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -25,149 +41,26 @@ public class App extends Application{
 		rootNode.setAlignment(Pos.CENTER);
 		
 		Scene myScene = new Scene(rootNode, 500, 500);
-		TextField inputField = new TextField();
-		Label input = new Label();
-		Button btn = new Button();
-		btn.setText("Type hexidecmial for result");
-		//launches app with button input
-		btn.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				ToBinary toBinary = new ToBinary();
-				String hexResult = toBinary.convertStringHex(inputField.getText());
-				input.setText(hexResult);
-			}
-		});
-		//number buttons
-		Button button1 = new Button();
-		Button button2 = new Button();
-		Button button3 = new Button();
-		Button button4 = new Button();
-		Button button5 = new Button();
-		Button button6 = new Button();
-		Button buttonAdd = new Button();
-		Button buttonSubtract = new Button();
-		Button buttonEqual = new Button();
-		Button buttonClear = new Button();
-		Button buttonBinary = new Button();
-		ButtonHandler buttonHandler = new ButtonHandler();
-		
-		//calculate constructor
-		Calculate calculate = new Calculate();
-		
-		//button text
-		button1.setText("1");
-		button2.setText("2");
-		button3.setText("3");
-		button4.setText("4");
-		button5.setText("5");
-		button6.setText("6");
-		buttonAdd.setText("+");
-		buttonSubtract.setText("-");
-		buttonEqual.setText("=");
-		buttonClear.setText("C");
-		buttonBinary.setText("To Binary");
-		//adding method for all number buttons to add value to them...
-		button1.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				String value = "1";
-				buttonHandler.addResult(value);
-				input.setText(buttonHandler.returnResult());
-			}
-			
-		});
-		button2.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				String value = "2";
-				buttonHandler.addResult(value);
-				input.setText(buttonHandler.returnResult());
-			}	
-		});
-		button3.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				String value = "3";
-				buttonHandler.addResult(value);
-				input.setText(buttonHandler.returnResult());
-			}
-		});
-		button4.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				String value = "4";
-				buttonHandler.addResult(value);
-				input.setText(buttonHandler.returnResult());
-			}
-		});
-		button5.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				String value = "5";
-				buttonHandler.addResult(value);
-				input.setText(buttonHandler.returnResult());
-			}
-		});
-		button6.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				String value = "6";
-				buttonHandler.addResult(value);
-				input.setText(buttonHandler.returnResult());
-			}
-		});
-		//button actions for operators
 
-		buttonAdd.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				String value = "+";
-				//calculate.init(buttonHandler.returnResult());
-				calculate.setOperator(value, buttonHandler.returnResult());
-				buttonHandler.clearResult();
-				input.setText("");
-			}
-		});
-		buttonSubtract.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				String value = "-";
-				//calculate.init(buttonHandler.returnResult());
-				calculate.setOperator(value, buttonHandler.returnResult());
-				buttonHandler.clearResult();
-				input.setText("");
-			}
-		});
-		buttonEqual.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				String numberSend = buttonHandler.returnResult();
-				int answer = calculate.calculate(numberSend);
-				String inputAnswer = Integer.toString(answer);
-				input.setText(inputAnswer);
-			}
-		});
-		buttonClear.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				buttonHandler.clearAll();
-				input.setText("");
-			}
-		});
-		buttonBinary.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				String result = calculate.toBinary();
-				input.setText(result);
-			}
-		});
+		buttonBinary.setOnAction(e -> buttonPressed(e));
 		
+		//adding method for all number buttons to add value to them...
+		button1.setOnAction(e -> buttonPressed(e)); // Sends the action event to the buttonPressed method
+		button2.setOnAction(e -> buttonPressed(e));
+		button3.setOnAction(e -> buttonPressed(e));
+		button4.setOnAction(e -> buttonPressed(e));
+		button5.setOnAction(e -> buttonPressed(e));
+		button6.setOnAction(e -> buttonPressed(e));
+		
+		//button actions for operators
+		buttonAdd.setOnAction(e -> buttonPressed(e));
+		buttonSubtract.setOnAction(e -> buttonPressed(e));
+		buttonEqual.setOnAction(e -> buttonPressed(e));
+		buttonClear.setOnAction(e -> buttonPressed(e));
 		//adding to rootnode
-		rootNode.add(btn, 0, 2);
+		rootNode.add(buttonBinary, 0, 2);
 		rootNode.add(input, 0, 0);
 		rootNode.add(inputField, 0, 1);
-		rootNode.add(buttonBinary, 0, 3);
 		rootNode.add(button1, 1, 0);
 		rootNode.add(button2, 2, 0);
 		rootNode.add(button3, 3, 0);
@@ -182,5 +75,70 @@ public class App extends Application{
 		primaryStage.show();
 	}
 	
+	public void buttonPressed(ActionEvent e)
+	{
+		String value = "";
+		
+		if(e.getSource() == button1){
+			value = "1";
+			buttonHandler.addResult(value);
+			input.setText(buttonHandler.returnResult());
+		}
+		else if(e.getSource() == button2){
+			value = "2";
+			buttonHandler.addResult(value);
+			input.setText(buttonHandler.returnResult());
+		}
+		else if(e.getSource() == button3){
+			value = "3";
+			buttonHandler.addResult(value);
+			input.setText(buttonHandler.returnResult());
+		}
+		else if(e.getSource() == button4){
+			value = "4";
+			buttonHandler.addResult(value);
+			input.setText(buttonHandler.returnResult());
+		}
+		else if(e.getSource() == button5){
+			value = "5";
+			buttonHandler.addResult(value);
+			input.setText(buttonHandler.returnResult());
+		}
+		else if(e.getSource() == button6){
+			value = "6";
+			buttonHandler.addResult(value);
+			input.setText(buttonHandler.returnResult());
+		}
+		else if(e.getSource() == buttonAdd){
+			value = "+";
+			//calculate.init(buttonHandler.returnResult());
+			calculate.setOperator(value, buttonHandler.returnResult());
+			buttonHandler.clearResult();
+			input.setText("");
+		}
+		else if(e.getSource() == buttonSubtract){
+			value = "-";
+			//calculate.init(buttonHandler.returnResult());
+			calculate.setOperator(value, buttonHandler.returnResult());
+			buttonHandler.clearResult();
+			input.setText("");
+		}
+		else if(e.getSource() == buttonEqual){
+			String numberSend = buttonHandler.returnResult();
+			int answer = calculate.calculate(numberSend);
+			String inputAnswer = Integer.toString(answer);
+			input.setText(inputAnswer);
+		}
+		else if(e.getSource() == buttonClear){
+			buttonHandler.clearAll();
+			input.setText("");
+		}
+		else if(e.getSource() == buttonBinary) {
+			String result = calculate.toBinary();
+			input.setText(result);
+			//ToBinary toBinary = new ToBinary();
+			//toBinary.decide(inputField.getText());
+		}
+	}
 
 }
